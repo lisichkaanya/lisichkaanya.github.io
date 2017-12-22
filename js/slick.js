36ec8a71c0eb479ce6e98cb1dbe807ac6950e173
lisichkaanya <34139104+lisichkaanya@users.noreply.github.com>
2017-12-22 09:44:02 +0200

    Add files via upload

---
+++ js/slick.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.sl').slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 2000,
+        cssEase: 'linear',
+        centerMode: true,
+        arrows: false,
+        variableWidth: true,
+    });
+    $('.sl1').slick({
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 2000,
+        cssEase: 'linear',
+        centerMode: true,
+        arrows: false,
+        variableWidth: true,
+    });
+});
